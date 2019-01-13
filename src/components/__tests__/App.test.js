11e4c33fc7043599be4939b170e75da8eec9d38e
Jerry Wang <jerryappleid761208@gmail.com>
2019-01-12 17:32:56 -0800

    intall enzyme

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
+import CommentBox from '../CommentBox';
 
 it('shows a comment box', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App /> , div);
   expect(div.innerHTML).toContain('CommentBox');
+  // expect(div).toHaveAnInstanceOf(CommentBox);
+  
   ReactDOM.unmountComponentAtNode(div);
 })

153eade9ba34e2c72d3838883e0498013fc8ce66
Jerry Wang <jerryappleid761208@gmail.com>
2019-01-12 19:57:16 -0800

    beforeEach

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -4,13 +4,19 @@ import App from 'components/App';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
-it('shows a comment box', () => {
-  const wrapped = shallow(<App />);
+let wrapped;
+
+/** beforeEach */
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
+
+it('shows a comment box', () => {  
   expect(wrapped.find(CommentBox).length).toEqual(1);
 })
 
-it('shows a comment list', () => {
-  const wrapped = shallow(<App />);
+it('shows a comment list', () => {  
   expect(wrapped.find(CommentList).length)
   .toEqual(1);
 })

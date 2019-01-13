b643a73e628110015619b4a4ee8cfa1a6ee49f17
Jerry Wang <jerryappleid761208@gmail.com>
2019-01-12 22:31:42 -0800

    textarea onChange text

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -16,3 +16,14 @@ it('has a textarea and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
 });
+
+it('has a text area that users can type in', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: { value: 'new comment'}
+  })
+  /** mock render */
+  wrapped.update()
+
+  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+
+});
